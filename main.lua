2c451214140a081bd16c848f5d393e8101a7be73
Uncontained0 <uncontained0@gmail.com>
2022-05-04 09:26:01 -0500

    idk

--- main.lua
+++ main.lua
@@ -7,8 +7,10 @@ local canvas2 = love.graphics.newCanvas(sizex,sizey)
 function love.load()
 	love.window.setMode(sizex*scale,sizey*scale)
 
-	shader:send("imageSize",{love.graphics.getDimensions()})
-	shader:send("r",2)
+	pcall(function()
+		shader:send("imageSize",{love.graphics.getDimensions()})
+		shader:send("r",25)
+	end)
 
 	love.graphics.setCanvas(canvas1)
 
@@ -18,15 +20,22 @@ function love.load()
 	love.graphics.setCanvas()
 end
 
+local t = 0
+function love.update(dt)
+	t = t + dt
+
+	if t > 1 then
+		print("update")
+
+		t = 0
+
+		canvas2:renderTo( function()
+			love.graphics.draw(canvas1)
+		end)
+	end
+end
+
 function love.draw()
 	love.graphics.scale(scale)
-
-	love.graphics.setCanvas(canvas2)
-	love.graphics.clear()
-	love.graphics.setShader(shader)
-	love.graphics.draw(canvas1)
-	love.graphics.setShader()
-	love.graphics.setCanvas()
-
-	canvas1,canvas2 = canvas2,canvas1
+	love.graphics.draw(canvas2)
 end
